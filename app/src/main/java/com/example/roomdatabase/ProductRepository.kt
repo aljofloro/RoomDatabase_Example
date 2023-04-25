3835b4d80e51fdeb6fec288c44296a0bac5ec1a3
aljofloro <aljofloro@gmail.com>
2023-04-24 19:53:41 -0500

    Initial commit

---
+++ app/src/main/java/com/example/roomdatabase/ProductRepository.kt
@@ -0,0 +1,36 @@
+package com.example.roomdatabase
+
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+
+class ProductRepository(private val productDao: ProductDao) {
+  val searchResults = MutableLiveData<List<Product>>()
+  private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+  fun insertProduct(newProduct: Product) {
+    coroutineScope.launch(Dispatchers.IO) {
+      productDao.insertProduct(newProduct)
+    }
+  }
+
+  fun deleteProduct(name: String) {
+    coroutineScope.launch(Dispatchers.IO) {
+      productDao.deleteProduct(name)
+    }
+  }
+
+  fun findProduct(name: String) {
+    coroutineScope.launch(Dispatchers.IO) {
+      searchResults.value = asyncFind(name).await()
+    }
+  }
+
+  private fun asyncFind(name: String): Deferred<List<Product>?> =
+    coroutineScope.async(Dispatchers.IO) {
+      return@async productDao.findProduct(name)
+    }
+}
